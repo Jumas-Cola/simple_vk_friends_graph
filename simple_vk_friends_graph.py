4a6410544020efbfb38ad7cd6eb54f2053cd2651
Jumas-Cola <kbbyfl91@gmail.com>
2018-09-19 23:42:04 +0300

    added mutual_friends

--- simple_vk_friends_graph.py
+++ simple_vk_friends_graph.py
@@ -19,7 +19,7 @@ class Drawer(object):
         }
         nx.draw(self.graph, pos=nx.spring_layout(self.graph), **options)
         # устанавливаем размер изображения в дюймах
-        plt.gcf().set_size_inches(150, 150)
+        plt.gcf().set_size_inches(40, 40)
         plt.savefig(self.file_name)
 
 
@@ -31,6 +31,10 @@ class Parser:
             user_id = person[0]['id']
         else:
             person = vk.method('users.get', {'user_ids': user_id})
+
+        person_name = person[0]['first_name']
+        person_last_name = person[0]['last_name']
+        person_id = str(person[0]['id'])
         # Получаем список друзей текущего пользователя
         friends = vk.method('friends.get', {'user_id': user_id})
         # Получаем информацию о друзьях текущего пользователя
@@ -39,9 +43,6 @@ class Parser:
         # Пишем друзей в файл
         f = open('grid.edgelist', 'a', encoding='utf-8')
         for friend in friends_info:
-            person_name = person[0]['first_name']
-            person_last_name = person[0]['last_name']
-            person_id = str(person[0]['id'])
             friend_name = friend['first_name']
             friend_last_name = friend['last_name']
             friend_id = str(friend['id'])
@@ -66,22 +67,66 @@ class Parser:
             except:
                 print('Cant get friends of id' + str(friend))
 
+    def mutual_friends(user_id=0):
+        # Получаем информацию о текущем пользователе
+        if not user_id:
+            person = vk.method('users.get')
+            user_id = person[0]['id']
+        else:
+            person = vk.method('users.get', {'user_ids': user_id})
+        person_name = person[0]['first_name']
+        person_last_name = person[0]['last_name']
+        person_id = str(person[0]['id'])
+        # Получаем список друзей текущего пользователя
+        friends = vk.method('friends.get', {'user_id': user_id})
+        # Получаем информацию о друзьях текущего пользователя
+        friends_info = vk.method('users.get', {'user_ids': ','.join([str(i) for i in friends['items']])})
+
+        # Пишем друзей в файл
+        f = open('grid.edgelist', 'a', encoding='utf-8')
+        for friend in friends_info:
+            friend_name = friend['first_name']
+            friend_last_name = friend['last_name']
+            friend_id = str(friend['id'])
+            node = '{}/{}/{}:{}/{}/{}:'.format(person_name,person_last_name,person_id,friend_name,friend_last_name,friend_id)+'{}\n'
+            f.write(node)
+            try:
+                # Получаем информацию о связях текущего друга
+                mutuals = vk.method('friends.getMutual', {'source_uid': person_id, 'target_uid': friend['id']})
+                mutuals_info = vk.method('users.get', {'user_ids': ','.join([str(i) for i in mutuals])})
+                for mutual in mutuals_info:
+                    mutual_name = mutual['first_name']
+                    mutual_last_name = mutual['last_name']
+                    mutual_id = str(mutual['id'])
+                    node = '{}/{}/{}:{}/{}/{}:'.format(friend_name,friend_last_name,friend_id,mutual_name,mutual_last_name,mutual_id)+'{}\n'
+                    f.write(node)
+            except:
+                print('Cant get mutuals of id'+str(person_id),'with id'+str(friend['id']))
+        f.close()
+
+
+
+
+
 
 login = 'login'
 password = 'password'
 user_id = 0	# id пользователя, для которого необходимо построить граф (0 - текущий пользователь)
-depth = 1	# глубина рекурсивного прохода по друзьям (больше 1 ставить не рекомендуется)
+depth = 1	# глубина рекурсивного прохода по друзьям
 users = [87896266, 174594973]
 
 # авторизация Вк
 vk = vk_api.VkApi(login=login, password=password)
 vk.auth()
 
+# определение перекрёстных связей среди друзей пользователя
+Parser.mutual_friends(user_id)
+
 # рекурсивный проход по друзьям, друзьям друзей и т.д.
-Parser.deep_friends(user_id, depth)
+# Parser.deep_friends(user_id, depth)
 
 # проход по заданному списку пользователей
-#Parser.users_by_list(users)
+# Parser.users_by_list(users)
 
 # чтение графа из файла
 G = nx.read_edgelist(path="grid.edgelist", delimiter=":")
