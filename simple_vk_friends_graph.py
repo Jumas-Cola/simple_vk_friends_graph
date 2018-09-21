6ecf6d7c33ef1f74619b65bbe61ad822743013d6
Jumas-Cola <kbbyfl91@gmail.com>
2018-09-21 14:03:08 +0300

    fixed mutual_friends_with_colors

--- simple_vk_friends_graph.py
+++ simple_vk_friends_graph.py
@@ -22,16 +22,15 @@ class Drawer(object):
         plt.gcf().set_size_inches(40, 40)
         plt.savefig(self.file_name)
 
-    def getRGBfromI(RGBint, scale=5, red=115, blue=218):
-        # scale масштаб (RGBint*scale) < 255
-        green = RGBint*scale & 255
+    def getRGBfromI(RGBint, maximum, red=115, blue=218):
+        green = int(RGBint*(255/maximum))
         return red, green, blue
 
-    def draw_with_colors(self):
+    def draw_with_colors(self, maximum):
         color_map = []
         for node in self.graph:
             power = int(node.split('/')[-1])
-            color_map.append('#%02x%02x%02x' % Drawer.getRGBfromI(power))
+            color_map.append('#%02x%02x%02x' % Drawer.getRGBfromI(power, maximum))
         options = {
             'node_color': color_map, # цвет узла
             'node_size': 3500, # размер узла
@@ -104,14 +103,14 @@ class Parser:
         # Получаем информацию о друзьях текущего пользователя
         friends_info = vk.method('users.get', {'user_ids': ','.join([str(i) for i in friends['items']])})
 
-        # Составляем список "популярности"
+        # Составляем словарь "популярности"
         powers={}
         for friend in friends['items']:
             try:
                 mutuals = vk.method('friends.getMutual', {'source_uid': person_id, 'target_uid': friend})
-                powers[str(friend)]=len(mutuals)
+                powers[str(friend)]={'count':len(mutuals), 'items':mutuals}
             except:
-                powers[str(friend)]=0
+                powers[str(friend)]={'count':0, 'items':[]}
 
         # Пишем друзей в файл
         f = open('grid.edgelist', 'a', encoding='utf-8')
@@ -119,21 +118,24 @@ class Parser:
             friend_name = friend['first_name']
             friend_last_name = friend['last_name']
             friend_id = str(friend['id'])
-            node = '{}/{}/{}/{}:{}/{}/{}/{}:'.format(person_name,person_last_name,person_id,0,friend_name,friend_last_name,friend_id,powers[friend_id])+'{}\n'
+            node = '{}/{}/{}/{}:{}/{}/{}/{}:'.format(person_name,person_last_name,person_id,0,friend_name,friend_last_name,friend_id,powers[friend_id]['count'])+'{}\n'
             f.write(node)
-            try:
-                # Получаем информацию о связях текущего друга
-                mutuals = vk.method('friends.getMutual', {'source_uid': person_id, 'target_uid': friend['id']})
+            # Получаем информацию о связях текущего друга
+            mutuals = powers[friend_id]['items']
+            if mutuals:
                 mutuals_info = vk.method('users.get', {'user_ids': ','.join([str(i) for i in mutuals])})
                 for mutual in mutuals_info:
                     mutual_name = mutual['first_name']
                     mutual_last_name = mutual['last_name']
                     mutual_id = str(mutual['id'])
-                    node = '{}/{}/{}/{}:{}/{}/{}/{}:'.format(friend_name,friend_last_name,friend_id,powers[friend_id],mutual_name,mutual_last_name,mutual_id,powers[mutual_id])+'{}\n'
+                    node = '{}/{}/{}/{}:{}/{}/{}/{}:'.format(friend_name,friend_last_name,friend_id,powers[friend_id]['count'],mutual_name,mutual_last_name,mutual_id,powers[mutual_id]['count'])+'{}\n'
                     f.write(node)
-            except:
-                print('Cant get mutuals of id'+str(person_id),'with id'+str(friend['id']))
         f.close()
+        maximum = 0
+        for i in powers:
+        	if powers[i]['count'] > maximum:
+        		maximum = powers[i]['count']
+        return maximum
 
     def mutual_friends(user_id=0):
         # Получаем информацию о текущем пользователе
@@ -187,11 +189,11 @@ if __name__ == "__main__":
     vk = vk_api.VkApi(login=login, password=password)
     vk.auth()
 
+    # определение перекрёстных связей с визуализацией популярности
+    maximum = Parser.mutual_friends_with_colors(user_id)
+
     # определение перекрёстных связей среди друзей пользователя
     #Parser.mutual_friends(user_id)
-
-    # определение перекрёстных связей с визуализацией популярности
-    Parser.mutual_friends_with_colors(user_id)
 
     # рекурсивный проход по друзьям, друзьям друзей и т.д.
     # Parser.deep_friends(user_id, depth)
@@ -206,4 +208,4 @@ if __name__ == "__main__":
     Graph = Drawer()
     Graph.graph = G
     #Graph.draw()
-    Graph.draw_with_colors()
+    Graph.draw_with_colors(maximum)
